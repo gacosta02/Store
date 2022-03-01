b06d5e6058847b7451f199c2ee335ad96cf2d17e
Rachelli Isela Matias Nuñez <rachelliisela303@gmail.com>
2022-02-28 23:09:53 -0400

    Nueva Solucion del Proyecto

---
+++ GalloStore/GalloStore/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using GalloStore.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GalloStore
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new MainPage();
+            // MainPage = new BienvenidaPage();
+            // MainPage = new LoginPage();
+            //MainPage = new RegistroPage();
+             MainPage = new MenuTabbedPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ GalloStore/GalloStore/Views/RegistroPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using GalloStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GalloStore.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistroPage : ContentPage
+    {
+        public RegistroPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new RegistroViewModel();
+        }
+    }
+}
