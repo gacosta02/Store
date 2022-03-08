9f9cb2e8f026922ea34dc8473fd4b085cebf1489
Rachelli Isela Matias Nuñez <rachelliisela303@gmail.com>
2022-03-07 22:44:23 -0400

    Pagina de catalogo y carrito

---
+++ GalloStore/GalloStore/Views/CarritoPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using GalloStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GalloStore.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CarritoPage : ContentPage
+    {
+        public CarritoPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new CarritoViewModel();
+        }
+    }
+}
--- GalloStore/GalloStore/Views/CatalogoPage.xaml.cs
+++ GalloStore/GalloStore/Views/CatalogoPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
 namespace GalloStore.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -17,6 +18,29 @@ namespace GalloStore.Views
         {
             InitializeComponent();
             this.BindingContext = new CatalogoViewModel();
+
         }
+            private void TapMenos_Tapped(object sender, EventArgs e)
+            {
+                int cantidad = Convert.ToInt32(lblCantidad);
+                if (cantidad > 1)
+                {
+
+                    cantidad -= 1;
+                    lblCantidad.Text = cantidad.ToString();
+                }
+
+            }
+
+            private void TapMas_Tapped(object sender, EventArgs e)
+            {
+                int cantidad = Convert.ToInt32(lblCantidad.Text);
+                cantidad += 1;
+                lblCantidad.Text = cantidad.ToString();
+            }
+
+        
+
+        
     }
 }
